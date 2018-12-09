64d4a76b4aea48f419c178e3bd9ac739bbe3539a
mohammadT77 <t.mohammadamin@gmail.com>
2018-12-09 11:44:51 +0330

    Update InstructionMemory.v
    -make in combinational

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -1,17 +1,14 @@
-module InstructionMemory(input clk, input reg [63:0] PC, output reg [31:0] instruction);
+module InstructionMemory(input reg [63:0] PC, output [31:0] instruction);
 	wire [31:0] addr1 = PC[63:32],
 		 addr2 = PC[31:0];
 	reg [31:0] IM [0:31][0:31];
-	reg [31:0] ins;
+	reg [32:0] ins;
 	initial begin
-		for (ins=0;ins<=32'hf;ins = ins + 1)
+		for (ins=0;ins<=32'hfffffff;ins = ins + 1)
 		begin
-			IM[0][ins] = ins;
-			IM[ins][0] = ins;
+			IM[32'd0][ins[31:0]] = ins[31:0];
+			IM[32'hffffffff][ins[31:0]] = ins[31:0];
 		end
 	end
-	always @(*)
-	begin
-		instruction = IM[addr1][addr2];
-	end
+	assign instruction = IM[addr1][addr2];
 endmodule	
