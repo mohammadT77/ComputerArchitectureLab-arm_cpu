5ec0800a605de30a275c9e3080e303b0b8097e90
mohammadT77 <t.mohammadamin@gmail.com>
2018-12-23 09:53:37 +0330

    Update ALUControl.v

--- ALUControl.v
+++ ALUControl.v
@@ -5,11 +5,11 @@ module ALUControl(input [31:0] instruction,input [1:0] ALUOp, output reg [3:0] A
 	2'b00: ALUctrl = 4'b0010;
 	2'b01: ALUctrl = 4'b0111;
 	2'b10: begin
-			case(instruction[28:26])
-			3'b000:ALUctrl = 4'b0010;
-			3'b010:ALUctrl = 4'b0110;
-			3'b100:ALUctrl = 4'b0000;
-			3'b101:ALUctrl = 4'b0001;
+			case(instruction[27:25])
+			3'b000:ALUctrl = 4'b0010; //ADD
+			3'b010:ALUctrl = 4'b0110; //SUB
+			3'b100:ALUctrl = 4'b0000; //AND
+			3'b101:ALUctrl = 4'b0001; //ORR
 			endcase
 		end
 	endcase
