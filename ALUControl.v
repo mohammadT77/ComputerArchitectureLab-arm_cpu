8390d91d653fc0a34e1e03537abad1d1b69eafec
mohammadT77 <t.mohammadamin@gmail.com>
2018-12-23 08:20:57 +0330

    Create ALUControl.v

---
+++ ALUControl.v
@@ -0,0 +1,17 @@
+module ALUControl(input [31:0] instruction,input [1:0] ALUOp, output reg [3:0] ALUctrl);
+
+	always @(instruction,ALUOp) begin
+	case(ALUOp)
+	2'b00: ALUctrl = 4'b0010;
+	2'b01: ALUctrl = 4'b0111;
+	2'b10: begin
+			case(instruction[28:26])
+			3'b000:ALUctrl = 4'b0010;
+			3'b010:ALUctrl = 4'b0110;
+			3'b100:ALUctrl = 4'b0000;
+			3'b101:ALUctrl = 4'b0001;
+			endcase
+		end
+	endcase
+	end
+endmodule
