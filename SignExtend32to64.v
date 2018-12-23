fc6c91d488e2cacfe027f048348baa822616db7d
mohammadT77 <t.mohammadamin@gmail.com>
2018-12-23 09:59:58 +0330

    Update SignExtend32to64.v

--- SignExtend32to64.v
+++ SignExtend32to64.v
@@ -1,3 +1,8 @@
-module SignExtend32to64(input [31:0] in, output [63:0] out);
-	assign out = { {32{in[31]}}  , in };
+module SignExtend32to64(input [31:0] ins, output reg [63:0] out);
+	always @(ins) begin
+		case(ins[31:29])
+		3'b010: out = { {55{ins[20]}}  , ins[20:12] };
+		3'b100: out = { {46{ins[23]}}  , ins[23:5] };
+		endcase
+	end
 endmodule
